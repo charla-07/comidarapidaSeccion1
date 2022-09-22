0170b4aef1a69d5d67d7cf878ea68e7f0436dca0
charla-07 <karlytaframa@hotmail.com>
2022-09-22 18:06:28 -0500

    “Subiendo”

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Menú</h1>
+    <hr />
+  <card url="https://images.pexels.com/photos/13573666/pexels-photo-13573666.jpeg?auto=compress&cs=tinysrgb&w=600" nombre="Vue.js" descripcion="descripcion" precio="$100.00"/> 
+  <card url="https://images.pexels.com/photos/13369646/pexels-photo-13369646.jpeg?auto=compress&cs=tinysrgb&w=600" nombre="Vue.js" descripcion="descripcion" precio="$150.00"/> 
+  <card url="https://images.pexels.com/photos/8463453/pexels-photo-8463453.jpeg?auto=compress&cs=tinysrgb&w=600" nombre="Vue.js" descripcion="descripcion" precio="$200.00"/> 
+  <card url="https://images.pexels.com/photos/8463464/pexels-photo-8463464.jpeg?auto=compress&cs=tinysrgb&w=600" nombre="Vue.js" descripcion="descripcion" precio="$350.00"/> 
+  <card url="https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&w=600" nombre="Vue.js" descripcion="descripcion" precio="$500.00"/> 
+  <card url="https://cdn.pixabay.com/photo/2017/11/25/17/17/sandwich-2977251__340.jpg" nombre="Vue.js" descripcion="descripcion" precio="$600.00"/>  
+</template>
+
+<script>
+  import Card from "./components/Card.vue";
+  export default{
+    components: { Card },
+  };
+</script>
+
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+    <grid>
+        <img v-bind:src="url" :alt="nombre">
+        <h3>{{ nombre }}</h3>
+        <p>{{ descripcion}}</p>
+        <h4>{{ precio }}</h4>
+    </grid>
+</template>
+
+<script>
+import Grid from "./Grid.vue";
+    export default {
+    props: {
+        url: { String, },
+        nombre: { String, },
+        descripcion: { String, },
+        precio: { String, },
+    },
+    components: { Grid }
+};
+</script>
+
+<style scoped>
+    img {
+        width: 10rem;
+        height: 15rem;
+        padding: 0.5rem;
+        background-color: snow;
+
+    }
+    h4 {
+        color:red;
+    }
+
+</style>
